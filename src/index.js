c5faabd30348260c582996a7aef437dce7fd3d85
RamanujKayal <itsmeramanuj@gmail.com>
2018-11-22 19:29:51 +0530

    uploaded 22.11.2018

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import React from 'react';
+/**
+ * ORIGINAL INDEX FILE START
+ */
+
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -9,4 +13,49 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();*/
+
+/**
+ * ORIGINAL INDEX FILE END
+ */
+
+/****************************************************************************** */
+
+ /**
+  * 'HELLO WORLD' PRINT START
+  */
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello World</h1>
+ReactDOM.render(element, document.getElementById('root'));*/
+
+/**
+  * 'HELLO WORLD' PRINT END
+  */
+
+/****************************************************************************** */
+
+/**
+ * JAVASCRIPT ESSENSIALS(SAMPLE CODES) START
+ */
+
+/** 1. DIFFERENCE BETWEEN VAR,LET,CONST */
+
+/*function sayHello(){
+
+  for(var i=0; i<5; i++ ){
+    console.log(i);
+  }
+}
+
+sayHello();*/
+
+
+
+  /**
+ * JAVASCRIPT ESSENSIALS(SAMPLE CODES) END
+ */
+
+/****************************************************************************** */
